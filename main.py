9bf4824e7ebe29503a0904917dc5071ca3d99c77
Oleg Stepanenkov <o.stepanenkov.95@gmail.com>
2025-11-25 08:13:37 +0300

    создана структура проекта и заполнены все файлы. middleware пока не прописаны в точке входа

---
+++ main.py
@@ -0,0 +1,49 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from config.config import Config, load_config
+from handlers.other import other_router
+from handlers.user import user_router
+from middlewares.inner import (
+    FirstInnerMiddleware,
+    SecondInnerMiddleware,
+    ThirdInnerMiddleware,
+)
+from middlewares.outer import (
+    FirstOuterMiddleware,
+    SecondOuterMiddleware,
+    ThirdOuterMiddleware,
+)
+
+# Инициализируем логгер модуля
+logger = logging.getLogger(__name__)
+
+
+# Функция конфигурирования и запуска бота
+async def main() -> None:
+
+    # Загружаем конфиг в переменную config
+    config: Config = load_config()
+
+    # Задаём базовую конфигурацию логирования
+    logging.basicConfig(
+        level=logging.getLevelName(level=config.log.level),
+        format=config.log.format,
+    )
+    # Инициализируем бот и диспетчер
+    bot = Bot(token=config.bot.token)
+    dp = Dispatcher()
+
+    # Регистрируем роутеры в диспетчере
+    dp.include_router(user_router)
+    dp.include_router(other_router)
+
+    # Здесь будем регистрировать миддлвари
+    # ...
+
+    # Запускаем polling
+    await dp.start_polling(bot)
+
+
+asyncio.run(main())
